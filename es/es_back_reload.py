d1eb6b6e62c542f9b7f73e7e41540bc75ecc7c4a
wangyong <vangyong@126.com>
2019-03-04 11:00:02 +0800

    xiugaidaoru

--- es/es_back_reload.py
+++ es/es_back_reload.py
@@ -5,8 +5,7 @@
 import configparser
 import csv
 import time
-import json
-import numpy as np
+import os
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 
@@ -16,7 +15,7 @@ es_url = cfg.get('db', 'es.url')
 es_index_pre = cfg.get('db', 'es.index.pre')
 es_type = cfg.get('db', 'es.type')
 es_backup = cfg.get('db', 'es.backup')
-es_relocation = cfg.get('db', 'es.relocation')
+es_reload = cfg.get('db', 'es.reload')
 
 es = Elasticsearch(hosts=es_url, http_auth=('', ''))
 query_json = {
@@ -33,28 +32,29 @@ class exportEsData():
         days = self.dateTrans(self.startTime, self.endTime)
         for d in days:
             query = es.search(index=es_index_pre + d + '*', body=query_json, scroll='5m', size=100)
+            if (query['hits']['total'] == 0):
+                print(" no data queryed :%s" % d)
+            else:
+                results = query['hits']['hits']  # es查询出的结果第一页
+                total = query['hits']['total']  # es查询出的结果总量
+                scroll_id = query['_scroll_id']  # 游标用于输出es查询出的所有结果
 
-            results = query['hits']['hits']  # es查询出的结果第一页
-            total = query['hits']['total']  # es查询出的结果总量
-            scroll_id = query['_scroll_id']  # 游标用于输出es查询出的所有结果
+                for i in range(0, int(total / 100) + 1):
+                    # scroll参数必须指定否则会报错
+                    query_scroll = es.scroll(scroll_id=scroll_id, scroll='5m')['hits']['hits']
+                    results += query_scroll
 
-            for i in range(0, int(total / 100) + 1):
-                # scroll参数必须指定否则会报错
-                query_scroll = es.scroll(scroll_id=scroll_id, scroll='5m')['hits']['hits']
-                results += query_scroll
-
-            with open('E:\data\es_back\logs_data-' + d + '.csv', 'w', newline='', encoding='utf-8') as flow:
-                csv_writer = csv.writer(flow)
-                for res in results:
-                    # print(res)
-                    # csv_writer.writerow([res['_id'] + ',' + res['_source']])
-                    source = res['_source']
-                    row_data = [res['_id'], source]
-                    csv_writer.writerow(row_data)
+                with open(es_backup + es_index_pre + d + '.csv', 'w', newline='', encoding='utf-8') as flow:
+                    csv_writer = csv.writer(flow)
+                    for res in results:
+                        # print(res)
+                        # csv_writer.writerow([res['_id'] + ',' + res['_source']])
+                        source = res['_source']
+                        row_data = [res['_id'], source]
+                        csv_writer.writerow(row_data)
 
     def dateTrans(self, startTime, endTime):
         days = set([])
-        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         tartTimeStr = time.strftime("%Y.%m.%d", time.localtime(startTime))
         endTimeStr = time.strftime("%Y.%m.%d", time.localtime(endTime))
         days.add(tartTimeStr)
@@ -62,7 +62,7 @@ class exportEsData():
         while startTime < endTime:
             startTime = startTime + 86400
             if (startTime < endTime):
-                tartTimeStr = time.strftime("%Y-%m-%d", time.localtime(startTime))
+                tartTimeStr = time.strftime("%Y.%m.%d", time.localtime(startTime))
                 days.add(tartTimeStr)
         return days
 
@@ -76,25 +76,27 @@ class importEsData():
     def importEsData(self):
         days = self.dateTrans(self.startTime, self.endTime)
         for d in days:
-            csv_file = csv.reader(open('E:\data\es_reload\logs_data-' + d + '.csv', 'r', encoding='UTF8'))
-            actions = []
-            for row in csv_file:
-                id = row[0]
-                source = row[1]
-                action = {
-                    "_index": es_index_pre + d,
-                    "_type": "logs",
-                    "_id": id,
-                    "_source": eval(source)
-                }
-                actions.append(action)
-            helpers.bulk(es, actions)
-            del actions[0:len(actions)]
-            print("import finish process:%s" % d)
+            if (os.path.exists(es_reload + es_index_pre + d + '.csv')):
+                csv_file = csv.reader(open(es_reload + es_index_pre + d + '.csv', 'r', encoding='UTF8'))
+                actions = []
+                for row in csv_file:
+                    id = row[0]
+                    source = row[1]
+                    action = {
+                        "_index": es_index_pre + d,
+                        "_type": "logs",
+                        "_id": id,
+                        "_source": eval(source)
+                    }
+                    actions.append(action)
+                helpers.bulk(es, actions)
+                del actions[0:len(actions)]
+                print("import finish process:%s" % d)
+            else:
+                print(" no data file day of :%s" % d)
 
     def dateTrans(self, startTime, endTime):
         days = set([])
-        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         tartTimeStr = time.strftime("%Y.%m.%d", time.localtime(startTime))
         endTimeStr = time.strftime("%Y.%m.%d", time.localtime(endTime))
         days.add(tartTimeStr)
@@ -102,13 +104,12 @@ class importEsData():
         while startTime < endTime:
             startTime = startTime + 86400
             if (startTime < endTime):
-                tartTimeStr = time.strftime("%Y-%m-%d", time.localtime(startTime))
+                tartTimeStr = time.strftime("%Y.%m.%d", time.localtime(startTime))
                 days.add(tartTimeStr)
         return days
 
-
 # 导出： 开始时间戳，结束时间戳 10位到秒
-# 导入： 先把文件放入对应的目录 如:E:\data\es_relocation\ 开始时间戳，结束时间戳
+# 导入： 先把文件放入对应的目录 如:E:\data\es_reload\ 开始时间戳，结束时间戳
 if __name__ == '__main__':
-    #  exportEsData(1545235200, 1545321600).exportData()
-    importEsData(1545235200, 1545321600).importEsData()
+    # exportEsData(1543593600, 1546185600).exportData()
+    importEsData(1543593600, 1546185600).importEsData()
