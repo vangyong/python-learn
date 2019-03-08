346f8c8cd40b18127fa7a5b6c3cfff2e90bb8849
wangyong <vangyong@126.com>
2019-03-08 17:09:00 +0800

    xiugai

--- es/es_back_restore.py
+++ es/es_back_restore.py
@@ -5,32 +5,62 @@
 import configparser
 import requests
 import json
+import time
+import logging
 
 cfg = configparser.ConfigParser()
 cfg.read('es.conf')
 es_url = cfg.get('db', 'es.url')
 es_index_pre = cfg.get('db', 'es.index.pre')
-es_type = cfg.get('db', 'es.type')
 es_backup = cfg.get('db', 'es.backup')
-es_reload = cfg.get('db', 'es.reload')
+
+
+def date_trans(startTime, endTime):
+    days = set([])
+    tartTimeStr = time.strftime("%Y.%m.%d", time.localtime(startTime))
+    endTimeStr = time.strftime("%Y.%m.%d", time.localtime(endTime))
+    days.add(tartTimeStr)
+    days.add(endTimeStr)
+    while startTime < endTime:
+        startTime = startTime + 86400
+        if (startTime < endTime):
+            tartTimeStr = time.strftime("%Y.%m.%d", time.localtime(startTime))
+            days.add(tartTimeStr)
+    return days
 
 
 def create_backup(startTime, endTime):
-    data = json.dumps({
-        "type": "fs",
-        "settings": {
-            "location": "/mount/backups/my_backup"
-        }
-    })
-    r = requests.put(es_url+'/_snapshot/my_backup', data)
-    print(r.content)
-    print("-----------------------")
-    print(r.content)
+    days = date_trans(startTime, endTime)
+    flag = 1
+    for d in days:
+        index = es_index_pre + d
+        r1 = requests.get(es_url + '/_snapshot' + es_backup + '/snapshot_' + d)
+        if (r1.status_code != 200):
+            json_data = json.dumps({"indices": index, "ignore_unavailable": "false", "include_global_state": "false"})
+            r = requests.put(es_url + '/_snapshot' + es_backup + '/snapshot_' + d, json_data)
+            if (r.status_code != 200):
+                logging.info("backup data failed day of " + d)
+                logging.info(r.content)
+                flag = 0
+    return flag
 
 
-# 导出： 开始时间戳，结束时间戳 10位到秒
-# 导入： 先把文件放入对应的目录 如:E:\data\es_reload\ 开始时间戳，结束时间戳
+def create_reload(startTime, endTime):
+    days = date_trans(startTime, endTime)
+    flag = 1
+    for d in days:
+        index = es_index_pre + d
+        json_data = json.dumps({"indices": index, "ignore_unavailable": "false", "include_global_state": "false"})
+        r = requests.put(es_url + '/_snapshot' + es_backup + '/snapshot_' + d + '/_restore', json_data)
+        if (r.status_code != 200):
+            logging.info("restore data failed day of " + d)
+            logging.info(r.content)
+            flag = 0
+    return flag
+
+
+# 备份： 开始时间戳，结束时间戳 10位到秒
+# 迁回： 先把文件放入对应的目录 如:/es_backup 开始时间戳，结束时间戳
 if __name__ == '__main__':
-    # exportEsData(1543593600, 1546185600).exportData()
-    # importEsData(1543593600, 1546185600).importEsData()
-    create_backup(1545321600, 1545321600)
+    create_backup(1545580800, 1545580800)
+    # create_reload(1545580800, 1545667200)
