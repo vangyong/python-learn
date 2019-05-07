69febd0eec3a3d7a84a2d64193c075b2e9189cd1
wangyong <vangyong@126.com>
2019-05-07 15:24:44 +0800

    修改

---
+++ pyshark/flask_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：pyshark_test.py
+#  yum install wireshark 或者 yum install tshark
+# pip install flask
+
+import pyshark
+import json
+
+from flask import Flask, abort, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/pcap/<id>', methods=['GET'])
+def get_task(id):
+    if not id:
+        return jsonify({'result': 'id is none'})
+    else:
+        fileCapture = pyshark.FileCapture('test.pcap')
+        packet_list = []
+        for packet in fileCapture:
+            packet_dict = {}
+            packet_dict['frame_info'] = packet.frame_info
+            layer_list = []
+            for layer in packet:
+                layer_dict = {}
+                layer_dict['layer_name'] = layer.layer_name
+                field_list = []
+                field_names = layer.field_names
+                for field_name in field_names:
+                    field_dict = {}
+                    field_dict['field_name'] = field_name
+                    field_dict['field_value'] = layer.get_field_value(field_name)
+                    field_list.append(field_dict)
+                layer_dict['field_list'] = field_list
+                layer_list.append(layer_dict)
+            packet_dict['layers'] = layer_list
+            packet_list.append(packet_dict)
+        return packet_list
+
+
+if __name__ == "__main__":
+    # 将host设置为0.0.0.0，则外网用户也可以访问到这个服务
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ pyshark/pyshark_test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：pyshark_test.py
+
+import pyshark
+import json
+import logging
+
+cap = pyshark.FileCapture('test.pcap')
+print("type of cap:")
+print(type(cap))
+print(cap)
+print('----------------------------------')
+print("type of cap[0]:")
+print(type(cap[0]))
+print(cap[0])
+print("type of cap[1]:")
+print(type(cap[1]))
+print(cap[1])
+print('----------------------------------')
+print("type of cap[0][0]:")
+print(type(cap[0][0]))
+print(cap[0][0])
+print('----------------------------------')
+print("type of cap[0][1]:")
+print(type(cap[0][1]))
+print(cap[0][1])
+print('----------------------------------')
+print("type of cap[0][1].layer_name")
+print(type(cap[0][1].layer_name))
+print(cap[0][1].layer_name)
+print('----------------------------------')
+print("type of cap[0][1].field_names")
+print(type(cap[0][1].field_names))
+print(cap[0][1].field_names)
+print('----------------------------------')
+print("type of cap[0][1].field_names[0]")
+print(type(cap[0][1].field_names[0]))
+print(cap[0][1].field_names[0])
