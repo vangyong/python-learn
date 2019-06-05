2ca1aa6dd6bf2ad80edd8b0def2e8a2731731cdf
wangyong <vangyong@126.com>
2019-06-05 19:58:29 +0800

    修改

--- message/nats_publish_test.py
+++ message/nats_publish_test.py
@@ -6,17 +6,20 @@ import asyncio
 import nats.aio.client
 import nats.aio.errors
 
+
 async def send_message(loop):
     mq_url = "nats://10.10.19.53:4222"
     client = nats.aio.client.Client()
     await client.connect(io_loop=loop, servers=[mq_url])
-    await client.publish("subject", "test1".encode())
+    await client.publish("subject", "this is my python message 中文消息".encode())
     await client.close()
+
 
 def main():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_message(loop))
     loop.close()
 
+
 if __name__ == '__main__':
-   main()
+    main()
--- message/nats_subscribe_test.py
+++ message/nats_subscribe_test.py
@@ -5,6 +5,7 @@
 import asyncio
 from nats.aio.client import Client as NATS
 
+
 async def run(loop):
     nc = NATS()
 
@@ -17,16 +18,7 @@ async def run(loop):
         print("Received a message on '{subject} {reply}': {data}".format(
             subject=subject, reply=reply, data=data))
 
-    # "*" matches any token, at any level of the subject.
-    # await nc.subscribe("foo.*.baz", cb=message_handler)
-    # await nc.subscribe("foo.bar.*", cb=message_handler)
-
-    # ">" matches any length of the tail of a subject, and can only be the last token
-    # E.g. 'foo.>' will match 'foo.bar', 'foo.bar.baz', 'foo.foo.bar.bax.22'
     await nc.subscribe("subject", cb=message_handler)
-
-    # Matches all of the above.
-    # await nc.publish("foo.bar.baz", b'Hello World')
 
     # Gracefully close the connection.
     await nc.drain()
